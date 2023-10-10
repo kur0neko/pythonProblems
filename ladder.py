cbab41c2cd61ff850bdaa68cda9437dfae8a8326
kur0neko <nttw648@gmail.com>
2023-10-10 10:46:20 -0700

    modify ladder.py and Merge_list function in Merge.py

--- ladder.py
+++ ladder.py
@@ -1,9 +1,7 @@
 def my_steps(n):
     if n<1 or n > 25:
             raise ValueError('n is out of boudary')
-    if n==1:
-       return n
-    elif n==2:
+    if n==1 or n==2:
         return n
     else:
         return my_steps(n-1)+my_steps(n-2)
--- merge.py
+++ merge.py
@@ -1,15 +1,15 @@
-def merge(list1, list2):
+def merge_list(list1, list2):
     i=0 
     j=0
-    mergeList = []
-    while i<len(list1) and j<len(list2):
+    myList = []
+    while i< len(list1) and j<len(list2):
         if list1[i] <= list2[j]:
-            mergeList.append(list1[i])  
+            myList.append(list1[i])  
             i += 1
             
         else:
-            mergeList.append(list2[j])
+            myList.append(list2[j])
             j += 1
-    mergeList.extend(list1[i:])  
-    mergeList.extend(list2[j:]) 
-    return mergeList
+    myList.extend(list1[i:])  
+    myList.extend(list2[j:]) 
+    return myList
