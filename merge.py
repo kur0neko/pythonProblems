f1ae687fcae96293862ec3ec6d7ee7099adfebfd
kur0neko <nttw648@gmail.com>
2023-10-10 11:00:08 -0700

    modify ladder.py and Merge_list function in Merge.py

--- merge.py
+++ merge.py
@@ -1,15 +1,17 @@
 def merge_list(list1, list2):
     i=0 
     j=0
-    myList = []
-    while i< len(list1) and j<len(list2):
-        if list1[i] <= list2[j]:
-            myList.append(list1[i])  
-            i += 1
-            
-        else:
-            myList.append(list2[j])
-            j += 1
-    myList.extend(list1[i:])  
-    myList.extend(list2[j:]) 
-    return myList
+    if isinstance(list1,list) and isinstance(list2,list):
+        myList = []
+        while i< len(list1) and j<len(list2):
+            if list1[i] <= list2[j]:
+                myList.append(list1[i])  
+                i += 1
+            else:
+                myList.append(list2[j])
+                j += 1
+        myList.extend(list1[i:])  
+        myList.extend(list2[j:]) 
+        return myList
+    else:
+        raise ValueError('invalid input')
