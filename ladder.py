f344d7572271c2d503bfdd17beff58f7a713e93c
kur0neko <nttw648@gmail.com>
2023-10-10 10:39:04 -0700

    modify ladder.py again

--- ladder.py
+++ ladder.py
@@ -1,23 +1,18 @@
-def fib(n):
-    if n <= 1:
-        return n
-    else:
-        return fib(n-1)+fib(n-2)
-    
-    
 def my_steps(n):
-    if n<1:
+    if n<1 or n > 25:
         try:
             raise ValueError('n is out of boudary')
         except ValueError as e:
             print(str(e))
-    elif n>25:
-        try:
-            raise ValueError('n is out of boudary')
-        except ValueError as e:
-            print(str(e)) 
+    if n==1:
+       return n
+    elif n==2:
+        return n
     else:
-        return fib(n+1)
+        return my_steps(n-1)+my_steps(n-2)
+    
+    
+    
 
 
         
