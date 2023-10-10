b31698cd9fd75a21ad1ed9b0b7f2a04870926f54
kur0neko <nttw648@gmail.com>
2023-10-10 10:40:30 -0700

    modify ladder.py again

--- ladder.py
+++ ladder.py
@@ -1,9 +1,6 @@
 def my_steps(n):
     if n<1 or n > 25:
-        try:
             raise ValueError('n is out of boudary')
-        except ValueError as e:
-            print(str(e))
     if n==1:
        return n
     elif n==2:
