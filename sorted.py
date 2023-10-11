bc07819a3ad006f06754d8ba2be722fa54d2b176
kur0neko <nttw648@gmail.com>
2023-10-11 15:07:17 -0700

    add sorted.py

---
+++ sorted.py
@@ -0,0 +1,10 @@
+def reverse_sort_dictionary(adict):
+    sorted_keys = sorted(adict.keys(), key = lambda x: adict[x][1])
+    
+    result = []
+    for k in sorted_keys:
+        
+        atuple = (k,adict[k][0])
+        result.append(atuple)
+        
+    return result
