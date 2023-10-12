186aabc8b257250306045f395e087650dd325da4
kur0neko <nttw648@gmail.com>
2023-10-12 13:28:31 -0700

    revert back merge.py

--- merge.py
+++ merge.py
@@ -10,17 +10,17 @@ def merge_list(list1, list2):
     list1_size=len(list1)
     list2_size=len(list2)
     
-    sorted_List1=[i for i, x in sorted(enumerate(list1), key=lambda x: x[1])]
-    sorted_list2=[i for i, x in sorted(enumerate(list2), key=lambda x: x[1])]
+    #sorted_List1=[i for i, x in sorted(enumerate(list1), key=lambda x: x[1])]
+    #sorted_list2=[i for i, x in sorted(enumerate(list2), key=lambda x: x[1])]
     myList = []
     
     while i< list1_size and j<list2_size:
-        if sorted_List1[i] <= sorted_List2[j]:
-            myList.append(sorted_List1[i])  
+        if list1[i] <= list2[j]:
+            myList.append(list1[i])  
             i += 1
         else:
-            myList.append(sorted_List2[j])
+            myList.append(list2[j])
             j += 1
-        myList.extend(sorted_List1[i:])  
-        myList.extend(sorted_List2[j:]) 
+        myList.extend(list1[i:])  
+        myList.extend(list2[j:]) 
         return myList
