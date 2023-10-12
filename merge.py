eb4cc0adad933ba40eb58bebad432c023b6b5d80
kur0neko <nttw648@gmail.com>
2023-10-12 14:19:17 -0700

    merge.py has been modified

--- merge.py
+++ merge.py
@@ -1,25 +1,58 @@
 def merge_list(list1, list2):
-    i=0 
-    j=0
-    
-    if not isinstance(list1, list) or not isinstance(list2, list):
-        raise TypeError("Invalide input, please enter type List only")
-    
-    list1_size=len(list1)
-    list2_size=len(list2)
-    myList = []
-    
-    while i< list1_size and j<list2_size:
-        if list1[i] <= list2[j]:
-            myList.append(list1[i])  
+    # Check if both inputs are lists
+    if not (isinstance(list1, list) and isinstance(list2, list)):
+        raise TypeError("Both inputs should be lists")
+
+    # Check if there are any non-integer elements in the input lists
+    for item in list1 + list2:
+        if not isinstance(item, int):
+            raise TypeError("Input lists should only contain integers")
+
+    # Merge the two lists
+    merged_list = list1 + list2
+
+    # Sort the merged list without using built-in sort functions
+    sorted_list = merge_sort(merged_list)
+
+    return sorted_list
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # Split the list into two halves
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    # Recursively sort both halves
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    # Merge the sorted halves
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
             i += 1
         else:
-            myList.append(list2[j])
+            merged.append(right[j])
             j += 1
-        myList.extend(list1[i:])  
-        myList.extend(list2[j:]) 
-        
-    return myList
 
-    
-    
+    # Append the remaining elements, if any
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
+
+# test
+#if __name__ == "__main__":
+   # list1 = [1, 5, 3, 7]
+   # list2 = [6, 2, 4]
+    #result = merge_list(list1, list2)
+    #print(result)
