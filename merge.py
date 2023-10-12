3fc98cbd213058067c2732621ef60b260ea0a8b0
kur0neko <nttw648@gmail.com>
2023-10-11 20:26:00 -0700

    edit merge.py

--- merge.py
+++ merge.py
@@ -4,28 +4,23 @@ def merge_list(list1, list2):
     if not isinstance(list1, list) or not isinstance(list2, list):
         raise TypeError("Invalide input, please enter type List only")
     
-    
+    #list1.sort()
+    #list2.sort()
+
     list1_size=len(list1)
     list2_size=len(list2)
     
-    sort_List1=[]
-    sort_List2=[]
-    for i in range(list1_size):
-            sort_List1.append(sorted(list1, key= lambda x:x[i]))
-        
-    for i in range(list2_size):
-            sort_List2.append(sorted(list2, key= lambda x:x[i]))    
-    
-  
+    sorted_List1=[i for i, x in sorted(enumerate(list1), key=lambda x: x[1])]
+    sorted_list2=[i for i, x in sorted(enumerate(list2), key=lambda x: x[1])]
     myList = []
     
-    while i< len(sort_List1) and j< len(sort_List2):
-        if sort_List1[i] <= sort_List1[j]:
-            myList.append(sort_List1[i])  
+    while i< list1_size and j<list2_size:
+        if sorted_List1[i] <= sorted_List2[j]:
+            myList.append(sorted_List1[i])  
             i += 1
         else:
-            myList.append(sort_List2)
+            myList.append(sorted_List2[j])
             j += 1
-        myList.extend(sort_List1[i:])  
-        myList.extend(sort_List2[j:]) 
+        myList.extend(sorted_List1[i:])  
+        myList.extend(sorted_List2[j:]) 
         return myList
