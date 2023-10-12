2a5564ac6a3e47c7c7776be49918e079d932bb79
kur0neko <nttw648@gmail.com>
2023-10-12 13:34:23 -0700

    edit merge.py

--- merge.py
+++ merge.py
@@ -4,22 +4,16 @@ def merge_list(list1, list2):
     if not isinstance(list1, list) or not isinstance(list2, list):
         raise TypeError("Invalide input, please enter type List only")
     
-    #list1.sort()
-    #list2.sort()
-
     list1_size=len(list1)
     list2_size=len(list2)
-    
-    #sorted_List1=[i for i, x in sorted(enumerate(list1), key=lambda x: x[1])]
-    #sorted_list2=[i for i, x in sorted(enumerate(list2), key=lambda x: x[1])]
     myList = []
     
-    while i< list1_size and j<list2_size:
+    while i< list1_size and j< list2_size:
         if list1[i] <= list2[j]:
             myList.append(list1[i])  
             i += 1
         else:
-            myList.append(list2[j])
+            myList.append(list2)
             j += 1
         myList.extend(list1[i:])  
         myList.extend(list2[j:]) 
