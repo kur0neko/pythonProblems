a3f519fb22dd5b294919eef7eb006170961ea5d9
kur0neko <nttw648@gmail.com>
2023-10-12 13:40:19 -0700

    another change in merge.py

--- merge.py
+++ merge.py
@@ -5,23 +5,20 @@ def merge_list(list1, list2):
     if not isinstance(list1, list) or not isinstance(list2, list):
         raise TypeError("Invalide input, please enter type List only")
     
-    else:
-        list1_size=len(list1)
-        list2_size=len(list2)
-        myList = []
-        
-        while i< list1_size and j<list2_size:
-            
-            if list1[i] <= list2[j]:
-                myList.append(list1[i])  
-                i += 1
-            else:
-                myList.append(list2[j])
-                j += 1
+    list1_size=len(list1)
+    list2_size=len(list2)
+    myList = []
+    while i< list1_size and j<list2_size:
+        if list1[i] <= list2[j]:
+            myList.append(list1[i])  
+            i += 1
+        else:
+            myList.append(list2[j])
+            j += 1
         myList.extend(list1[i:])  
         myList.extend(list2[j:]) 
         
-        return myList
+    return myList
 
     
     
